d04ab2ad22321289c952b1e2a9da997b0a7c884c
Naru-hub <1208ny0324@gmail.com>
2021-10-10 12:16:11 +0000

    add 読書ログ残りの項目の登録表示

--- book_log.php
+++ book_log.php
@@ -1,19 +1,29 @@
 <?php
 
 echo '読書ログを登録してください' . PHP_EOL;
+
 echo '書籍名 : ';
 $title = trim(fgets(STDIN));
+
+echo '著者名 : ';
+$author = trim(fgets(STDIN));
+
+echo '読書状況(未読,読んでる,読了) : ';
+$status = trim(fgets(STDIN));
+
+echo '評価(5点満点の整数) : ';
+$review = trim(fgets(STDIN));
+
+echo '感想 : ';
+$comment = trim(fgets(STDIN));
+
 
 echo '登録が完了しました' . PHP_EOL .PHP_EOL;
 
 echo '読書ログを表示します' . PHP_EOL;
 echo '書籍名 : ' . $title . PHP_EOL;
+echo '著者名 : ' . $author . PHP_EOL;
+echo '読書状況 : ' . $status . PHP_EOL;
+echo '評価 : ' . $review . PHP_EOL;
+echo '感想 : '  .$comment . PHP_EOL;
 
-
-
-
-
-// echo '著者名 : 宮沢賢治' . PHP_EOL;
-// echo '読書状況 : 読了' . PHP_EOL;
-// echo '評価 : ５' . PHP_EOL;
-// echo '感想 : 本当の幸せとは何だろうかと考えさせられる作品だった。' . PHP_EOL;
