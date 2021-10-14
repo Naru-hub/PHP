6abb789b2ffb48c15748b571325aee9785c25ff6
Naru-hub <1208ny0324@gmail.com>
2021-10-14 13:26:04 +0000

    add foreach 配列と連想配列

--- loop.php
+++ loop.php
@@ -1,17 +1,35 @@
 <?php
-//無限ループwhile
 
-$i = 0;
+// $numbers = [1,2,3,4,5];
 
-while (true) {
-  //50より少なければ
-  if ($i <= 50) {
-      //10ずつ増やし、数値を出力する
-      // $i = $i + 10;
-      echo $i . PHP_EOL;
-      $i += 10;
-  } else {
-      //ループ処理を終了する
-      break;
+// foreach ($numbers as $number) {
+//   echo $number * 2 .PHP_EOL;
+// }
+
+$currencies = [
+  'japan' => 'yen',
+  'us' => 'dollar',
+  'england' => 'pound',
+  ];
+  foreach($currencies as $country => $currency) {
+    echo $country . ': '. $currency .PHP_EOL;
   }
-}
+
+
+
+
+//無限ループwhile
+// $i = 0;
+
+// while (true) {
+//   //50より少なければ
+//   if ($i <= 50) {
+//       //10ずつ増やし、数値を出力する
+//       // $i = $i + 10;
+//       echo $i . PHP_EOL;
+//       $i += 10;
+//   } else {
+//       //ループ処理を終了する
+//       break;
+//   }
+// }
