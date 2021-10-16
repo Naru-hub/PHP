382c849805aaf19e76cd44541b20332280c59eb6
Naru-hub <1208ny0324@gmail.com>
2021-10-16 12:55:03 +0000

    add 関数　読書ログ表示

--- book_log.php
+++ book_log.php
@@ -2,31 +2,45 @@
 
 function createReview()
 {
-  echo '読書ログを登録してください' . PHP_EOL;
-      echo '書籍名 : ';
-      $title = trim(fgets(STDIN));
+    echo '読書ログを登録してください' . PHP_EOL;
+    echo '書籍名 : ';
+    $title = trim(fgets(STDIN));
+    
+    echo '著者名 : ';
+    $author = trim(fgets(STDIN));
+    
+    echo '読書状況(未読,読んでる,読了) : ';
+    $status = trim(fgets(STDIN));
+    
+    echo '評価(5点満点の整数) : ';
+    $score = trim(fgets(STDIN));
+    
+    echo '感想 : ';
+    $summary = trim(fgets(STDIN));
+    
+    echo '登録が完了しました' . PHP_EOL .PHP_EOL;
+    
+    return [
+      'title' => $title,
+      'author' => $author,
+      'status' => $status,
+      'score' => $score,
+      'summary' => $summary,
+      ];
+}
+
+function listReviews($reviews) 
+{
+    echo '登録されている読書ログを表示します' . PHP_EOL;
       
-      echo '著者名 : ';
-      $author = trim(fgets(STDIN));
-      
-      echo '読書状況(未読,読んでる,読了) : ';
-      $status = trim(fgets(STDIN));
-      
-      echo '評価(5点満点の整数) : ';
-      $score = trim(fgets(STDIN));
-      
-      echo '感想 : ';
-      $summary = trim(fgets(STDIN));
-      
-      echo '登録が完了しました' . PHP_EOL .PHP_EOL;
-      
-      return [
-        'title' => $title,
-        'author' => $author,
-        'status' => $status,
-        'score' => $score,
-        'summary' => $summary,
-        ];
+    foreach($reviews as $review) {
+        echo '書籍名 : ' . $review['title'] . PHP_EOL;
+        echo '著者名 : ' . $review['author'] . PHP_EOL;
+        echo '読書状況 : ' . $review['status'] . PHP_EOL;
+        echo '評価 : ' . $review['score'] . PHP_EOL;
+        echo '感想 : '  .$review['summary'] . PHP_EOL;
+        echo '____________' . PHP_EOL;
+    }
 }
 
 $reviews = [];
@@ -39,22 +53,11 @@ while (true) {
     $num = trim(fgets(STDIN));
 
     if ($num === '1') {
-      $reviews[] = createReview();
-      
-      
+        $reviews[] = createReview();
     } elseif ($num === '2'){
-      echo '登録されている読書ログを表示します' . PHP_EOL;
-      
-      foreach($reviews as $review) {
-          echo '書籍名 : ' . $review['title'] . PHP_EOL;
-          echo '著者名 : ' . $review['author'] . PHP_EOL;
-          echo '読書状況 : ' . $review['status'] . PHP_EOL;
-          echo '評価 : ' . $review['score'] . PHP_EOL;
-          echo '感想 : '  .$review['summary'] . PHP_EOL;
-          echo '____________' . PHP_EOL;
-      }
+        listReviews($reviews);
     } elseif ($num === '9') {
-      break;
+        break;
     }
 }
   
