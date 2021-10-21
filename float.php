2ee7ec69d33e9321cb299b55ea2ded2d2569ae67
Naru-hub <1208ny0324@gmail.com>
2021-10-21 14:28:02 +0000

    add 浮動小数点

---
+++ float.php
@@ -0,0 +1,8 @@
+<?php
+var_dump(1.1);
+var_dump(1.1e2);
+var_dump(0.1 + 0.2);
+var_dump((0.1 + 0.2) === 0.3);
+
+// $number = bcadd('0.1','0.2',1);        bcmath関数
+// var_dump((float) $number === 0.3);
