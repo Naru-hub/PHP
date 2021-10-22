ada8e45735f07673fc21c9ef04f302c8483625a8
Naru-hub <1208ny0324@gmail.com>
2021-10-22 14:44:44 +0000

    phpの型その他

--- sample3.php
+++ sample3.php
@@ -1,7 +1,31 @@
 <?php
 
-if (null) {
-  echo 'true' .PHP_EOL;
-} else {
-  echo 'false' .PHP_EOL;
-}
+// if (null) {
+//   echo 'true' .PHP_EOL;
+// } else {
+//   echo 'false' .PHP_EOL;
+// }
+
+// $a = null;
+// unset($a);
+// var_dump($a);
+
+// class Member
+// {
+//   function getName()
+//   {
+//     echo "名前を返します";
+//   }
+// }
+
+// $member = new Member;
+// echo gettype($member);
+
+$triple = function ($int) {
+  return $int * 3;
+};
+
+$numbers = array_map($triple,[1,2,3]);
+
+var_dump(implode('',$numbers));
+var_dump(is_callable($triple));
