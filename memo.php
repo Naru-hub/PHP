9994cafd97d58c3abea94f3a7a6b0807835255f1
Naru-hub <1208ny0324@gmail.com>
2021-10-24 06:47:38 +0000

    add Q2.php

--- memo.php
+++ memo.php
@@ -1,7 +1,14 @@
  <?php
  
- echo 'メモのタイトルを入れてください。' .PHP_EOL;
+ echo 'メモのタイトルを入れてください。' . PHP_EOL;
  echo 'タイトル : ';
- $title = fgets(STDIN) .PHP_EOL;
+ $title = trim(fgets(STDIN)) .PHP_EOL;
+ 
+ echo 'メモの内容を入れてください。' . PHP_EOL;
+ echo '内容 : ';
+ $contents = fgets(STDIN) . PHP_EOL;
+ 
+ echo '登録が完了しました。' . PHP_EOL;
  
  echo 'タイトル :' . $title;
+ echo '内容 :' . $contents;
