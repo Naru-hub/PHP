cfe71cd4c65859818bb75785266f51c1098e5d41
Naru-hub <1208ny0324@gmail.com>
2021-10-24 08:20:48 +0000

    配列　asort sort

---
+++ array5.php
@@ -0,0 +1,23 @@
+<?php
+
+$weights = [
+    'Kumagae' => 50,
+    'Suemitsu' => 90,
+    'Kawasaki' => 45,
+  ];
+  
+  sort($weights);
+  // asort($weights);
+  var_dump($weights);
+
+// $movie = [
+//     'name' => 'totoro',
+//   ];
+// $movie['year'] = 1988;
+
+// $movie2 = [
+//     'director' => 'Miyazaki Hayao',
+//   ];
+  
+// $movie3 = array_merge($movie, $movie2);
+// var_dump(count($movie3));
