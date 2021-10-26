443242e94f349cb32ce39096eb1110cdb1e6739f
Naru-hub <1208ny0324@gmail.com>
2021-10-26 14:29:10 +0000

    配列

--- array5.php
+++ array5.php
@@ -1,14 +1,14 @@
 <?php
 
-$weights = [
-    'Kumagae' => 50,
-    'Suemitsu' => 90,
-    'Kawasaki' => 45,
-  ];
+// $weights = [
+//     'Kumagae' => 50,
+//     'Suemitsu' => 90,
+//     'Kawasaki' => 45,
+//   ];
   
-  sort($weights);
-  // asort($weights);
-  var_dump($weights);
+//   sort($weights);
+//   // asort($weights);
+//   var_dump($weights);
 
 // $movie = [
 //     'name' => 'totoro',
@@ -21,3 +21,36 @@ $weights = [
   
 // $movie3 = array_merge($movie, $movie2);
 // var_dump(count($movie3));
+
+// function square($n)
+// {
+//     return $n * $n;
+// }
+
+// $arr = [2,4,6];
+// $newArr = array_map('square', $arr);
+// print_r($newArr);
+
+// function even($n)
+// {
+//     return ($n % 2) ===0;
+// }
+
+// $arr = [1,2,3,4,5];
+// $newArr = array_filter($arr,'even');
+// print_r($newArr);
+
+$userAges = [
+        'Tanaka' => 20,
+        'kimura' => 25,
+        'Tabata' => 40,
+    ];
+    
+    $userOver30 = array_filter
+    ($userAges, function ($v) {
+        return $v >= 30;
+    });
+    
+    print_r($userOver30);
+    
+    
