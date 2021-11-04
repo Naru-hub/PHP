63203a217ee8bb427cc9928af1713550df3d1de0
Naru-hub <1208ny0324@gmail.com>
2021-11-04 13:42:45 +0000

    制御構造

--- sample.php
+++ sample.php
@@ -1,14 +1,14 @@
 <?php
 
-echo gettype(1) . PHP_EOL;
-echo gettype(0) . PHP_EOL;
-echo gettype(-1) . PHP_EOL;
+// echo gettype(1) . PHP_EOL;
+// echo gettype(0) . PHP_EOL;
+// echo gettype(-1) . PHP_EOL;
 
-var_dump(0b10);
-var_dump(010);
-var_dump(0x10);
-var_dump(PHP_INT_MAX);
-var_dump((PHP_INT_MAX+1) === (PHP_INT_MAX+2));
+// var_dump(0b10);
+// var_dump(010);
+// var_dump(0x10);
+// var_dump(PHP_INT_MAX);
+// var_dump((PHP_INT_MAX+1) === (PHP_INT_MAX+2));
 
 // $a = 'php';
 // $b = $a;
@@ -24,3 +24,21 @@ var_dump((PHP_INT_MAX+1) === (PHP_INT_MAX+2));
 
 // echo $c . PHP_EOL;
 // echo $d . PHP_EOL;
+
+$numbers = [1,2,3];
+$doublenumbers = [];
+
+foreach ($numbers as $number) {
+  $doublenumbers[] = $number * 2;
+}
+
+print_r($doublenumbers);
+
+
+
+function double($i)
+{
+  return $i*2;
+}
+$n = double(3);
+echo $n; //
