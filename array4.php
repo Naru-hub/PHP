8729a8590c82330dd7e09b22b238b3895fb998dd
Naru-hub <1208ny0324@gmail.com>
2021-11-08 14:33:25 +0000

    array

--- array4.php
+++ array4.php
@@ -10,3 +10,23 @@ $members = [
 
 $member = ['T','A'];
 var_dump($members);
+
+
+$fruits = array(
+    "fruits" => array("a" => "orange", "b" => "banana", "c" => "apple"),
+    "numbers" => array(1,2,3,4,5,6),
+    "holes" => array("first", 5 => "second", "third")
+    );
+    print_r($fruits);
+    
+    $array = array(1, 1, 1, 1,  1, 8 => 1, 4 => 1, 19, 3 => 13);
+    print_r($array);
+    
+    
+    $firstquarter = array(1 => 'January', 'February', 'March');
+    print_r($firstquarter);
+    
+    $foo = array('bar' => 'baz');
+    echo "Hello {$foo['bar']}!";
+    
+    
