a13be88a64b637b24f60a1477e45f6aa0a17150e
Naru-hub <1208ny0324@gmail.com>
2021-11-10 14:28:30 +0000

    do-while

--- while.php
+++ while.php
@@ -26,18 +26,60 @@
 //     $x += 10;
 //   }
   
-  $arr = array("orange","banana","apple","raspberry");
+  // $arr = array("orange","banana","apple","raspberry");
   
-  $i = 0;
-  while ($i < count($arr)) {
-    $a = $arr[$i];
-    echo $a . "\n";
-    $i++;
-  }
+  // $i = 0;
+  // while ($i < count($arr)) {
+  //   $a = $arr[$i];
+  //   echo $a . "\n";
+  //   $i++;
+  // }
   
-  $myArray = array('aa','bb','cc','dd');
-  while (list ($key, $val) = each ($myArray)) echo $val;
-  reset($myArray);
-  while (list ($key, $val) = each ($myArray)) echo $val;
+  // $myArray = array('aa','bb','cc','dd');
+  // while (list ($key, $val) = each ($myArray)) echo $val;
+  // reset($myArray);
+  // while (list ($key, $val) = each ($myArray)) echo $val;
   
   
+//do-while
+
+// $i =0;
+// do {
+//   echo $i;
+// } while ($i > 0);
+
+// do {
+//     if ($i < 5) {
+//       echo "iは十分大きくありません。";
+//       break;
+//     }
+//     $i *= $factor;
+//     if ($i < $minimum_limit) {
+//         break;
+//     }
+//     echo "iはOKです。";
+    
+// } while (0);
+
+$numbers = array();
+$array_size = 10;
+
+for ($i = 0; $i < $array_size; $i++) {
+  do {
+    $random = rand(1,1000);
+  } while (($random % 2) == 1);
+  $numbers[] = $random;
+}
+
+asort($numbers);
+
+echo '<pre>';
+print_r($numbers);
+echo '</pre>';
+
+$x = 1;
+
+do {
+  echo "The number is: $x <br>";
+  $x++;
+} while ($x <= 5);
